43c45d12e5e010fb955c58ab3920dcf5011526b9
timhamrouge <t.hamrouge@gmail.com>
2020-06-17 16:35:50 +0100

    user can add todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import ToDoList from "./components/ToDoList"
-import {hot} from "react-hot-loader";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
-class App extends Component{
+
+class App extends Component {
   state = {
-    todos: ['1', '2', '3']
+    todos: ['1', '2', '3'],
+    newTodoName: ''
   }
-  render(){
-    const { todos } = this.state;
-    console.log(todos)
-    return(
+
+  addTodo = (e) => {
+    console.log('called')
+    e.preventDefault();
+    console.log(this.state.todos)
+    let { todos, newTodoName } = this.state;
+    todos.push(newTodoName);
+    // const todos = this.state.todos.push(this.state.newTodoName);
+    this.setState({todos})
+  };
+
+  handleNameField = (e) => {
+    console.log(e.target.value)
+    this.setState({ newTodoName: e.target.value })
+  }
+
+  render() {
+    const { todos, newTodoName } = this.state;
+    // console.log(this.state)
+    return (
       <div className="App">
-        <ToDoList 
-          todos={todos}
-        />
+
+        <form>
+          <label htmlFor="add-todo">First name:</label><br/>
+            <input type="text" id="fname" name="add-todo" value={newTodoName} onChange={(e) => this.handleNameField(e)}>
+              </input>
+            <br/>
+                  <input onClick={ (e) => this.addTodo(e) } type="submit" value="Submit">
+                    </input>
+          </form>
+
+                  <ToDoList
+                    todos={todos}
+                  />
       </div>
     );
   }
+
+
 }
 
 export default hot(module)(App);
